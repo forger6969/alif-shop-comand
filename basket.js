9fa1625b753c2402b0156080f79c7b5969f3f724
unknown <javohirgithub@gmail.comcleargit config --global user.email javohirgithub@gmail.com>
2025-11-29 17:29:37 +0500

    kozim chala

---
+++ basket.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+    function plus() {
+        count++;
+        document.getElementById("count").textContent = count;
+    }
+
+    function minus() {
+        if (count > 0) {
+            count--;
+        }
+        document.getElementById("count").textContent = count;
+    }
